1f501bfc978f58d26ef2c33f4b63e62af16237fa
kroonprins <marcodebac@gmail.com>
2020-03-08 16:29:34 +0100

    first step for sensible defaults

--- src/core/transform.ts
+++ src/core/transform.ts
@@ -1,16 +1,19 @@
 import { Definition, TransformedDefinition } from "./model";
 import { transformers } from "./registration";
+import { Action, CommonArguments } from "./actions/model";
 
-const transform = (definitions: Definition[]): TransformedDefinition[] => {
-    return definitions
+const transform = (definitions: Definition[], action: Action, args: CommonArguments) => {
+    return Promise.all(
+        definitions
         .map(definition => {
             const transformer = transformers()
-                .find(transformer => transformer.canTransform(definition))
+                .find(transformer => transformer.canTransform(definition, action, args))
             if (!transformer) {
                 throw new Error(`TODO no transformer registered`)
             }
-            return transformer.transform(definition)
+            return transformer.transform(definition, action, args)
         })
+    )
 }
 
 export {
