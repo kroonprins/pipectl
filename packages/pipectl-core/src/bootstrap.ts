512c286d6bebd52742c92e01a21734d7992462e1
kroonprins <marcodebac@gmail.com>
2020-04-08 16:29:33 +0200

    dqfcccccc

--- packages/pipectl-core/src/bootstrap.ts
+++ packages/pipectl-core/src/bootstrap.ts
@@ -1,16 +1,52 @@
 import { Command } from 'commander'
+import apply from './actions/apply'
+import _delete from './actions/delete'
+import get from './actions/get'
 import { DuplicateFilter } from './filters/DuplicateFilter'
 import { SelectorFilter } from './filters/SelectorFilter'
 import { registerFallbackReporter, registerFilter } from './registration'
 import { FallbackReporter } from './reporter'
+import { multiple, registerCommand } from './util/commander'
 
 type BootstrapFunction = (command: Command) => void
 
-const bootstrapCore: BootstrapFunction = (_command: Command) => {
-  registerFilter(new SelectorFilter())
-  registerFilter(new DuplicateFilter())
+const bootstrapCore: BootstrapFunction = (program: Command) => {
+  registerCommand(
+    program
+      .command('apply')
+      .requiredOption('-f, --filename <fileOption>', 'File name to apply.', multiple)
+      .option('-R, --recursive', 'recursively if directory')
+      .option('--dry-run', 'todo')
+      .option('-l, --selector <selector>', 'todo')
+      .option('-n, --namespace <namespace>', 'todo')
+      .option('-o, --output <output>', 'todo')
+      .action(apply),
+    program
+      .command('delete')
+      .requiredOption('-f, --filename <fileOption>', 'File name to apply.', multiple)
+      .option('-R, --recursive', 'recursively if directory')
+      .option('--dry-run', 'todo')
+      .option('-l, --selector <selector>', 'todo')
+      .option('-n, --namespace <namespace>', 'todo')
+      .option('-o, --output <output>', 'todo')
+      .action(_delete),
+    program
+      .command('get <kind> [name]')
+      .option('-l, --selector <selector>', 'todo')
+      .option('-n, --namespace <namespace>', 'todo')
+      .option('-o, --output <output>', 'todo')
+      .option('--export', 'todo')
+      .action(get)
+  )
 
-  registerFallbackReporter(new FallbackReporter())
+  registerFilter(
+    new SelectorFilter(),
+    new DuplicateFilter()
+  )
+
+  registerFallbackReporter(
+    new FallbackReporter()
+  )
 }
 
 export { BootstrapFunction, bootstrapCore }
--- packages/pipectl/src/index.ts
+++ packages/pipectl/src/index.ts
@@ -1,12 +1,8 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander'
-import apply from 'pipectl-core/dist/actions/apply'
-import _delete from 'pipectl-core/dist/actions/delete'
-import get from 'pipectl-core/dist/actions/get'
 import { bootstrapCore, BootstrapFunction } from 'pipectl-core/dist/bootstrap'
 import { currentPlugins, initialize as initializeConfig } from 'pipectl-core/dist/config'
-import { addCommands, multiple } from 'pipectl-core/dist/util/commander'
 import { initializeLogging, log } from 'pipectl-core/dist/util/logging'
 
 (async () => {
@@ -15,33 +11,6 @@ import { initializeLogging, log } from 'pipectl-core/dist/util/logging'
     initializeConfig()
 
     const program = new Command()
-    addCommands(
-      program
-        .command('apply')
-        .requiredOption('-f, --filename <fileOption>', 'File name to apply.', multiple)
-        .option('-R, --recursive', 'recursively if directory')
-        .option('--dry-run', 'todo')
-        .option('-l, --selector <selector>', 'todo')
-        .option('-n, --namespace <namespace>', 'todo')
-        .option('-o, --output <output>', 'todo')
-        .action(apply),
-      program
-        .command('delete')
-        .requiredOption('-f, --filename <fileOption>', 'File name to apply.', multiple)
-        .option('-R, --recursive', 'recursively if directory')
-        .option('--dry-run', 'todo')
-        .option('-l, --selector <selector>', 'todo')
-        .option('-n, --namespace <namespace>', 'todo')
-        .option('-o, --output <output>', 'todo')
-        .action(_delete),
-      program
-        .command('get <kind> [name]')
-        .option('-l, --selector <selector>', 'todo')
-        .option('-n, --namespace <namespace>', 'todo')
-        .option('-o, --output <output>', 'todo')
-        .option('--export', 'todo')
-        .action(get)
-    )
 
     bootstrapCore(program)
 
