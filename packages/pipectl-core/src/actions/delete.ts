0ec87bec4df60ce8ec2fb436b275027cec2789b9
kroonprins <marcodebac@gmail.com>
2020-04-13 16:24:28 +0200

    dkqsfljf

--- packages/pipectl-core/src/actions/delete.ts
+++ packages/pipectl-core/src/actions/delete.ts
@@ -1,12 +1,12 @@
 import { process } from '../process'
 import { log } from '../util/logging'
-import { inputDefinitions as inputDefinitionsFromYaml } from '../util/yaml'
+import { inputDefinitions as inputDefinitionsFromFile } from '../files'
 import { Action, ApplyArguments } from './model'
 import { stringifyApplyArguments } from './util'
 
 export default async (args: ApplyArguments) => {
   log.debug(`Action apply '${stringifyApplyArguments(args)}'`)
-  const definitions = await inputDefinitionsFromYaml(args)
+  const definitions = await inputDefinitionsFromFile(args)
   return process(definitions, Action.DELETE, args)
 }
 
---
+++ packages/pipectl-core/src/files/model.ts
@@ -0,0 +1,24 @@
+import { CommonArguments as Arguments } from '../actions/model'
+import { Definition } from '../model'
+
+class FileName {
+  public extension: string | undefined
+  constructor(public name: string) {
+    this.extension = name.split('.').pop()
+  }
+}
+
+interface DefinitionFile {
+  name?: FileName,
+  content: string,
+}
+
+type FileOptionHandler = (fileOption: string, args: Arguments) => Promise<DefinitionFile[]>
+
+interface DefinitionFileHandler {
+  canHandle(file: DefinitionFile): boolean
+  handle(file: DefinitionFile): Promise<Definition[]>
+}
+
+export { FileName, DefinitionFile, FileOptionHandler, DefinitionFileHandler }
+
