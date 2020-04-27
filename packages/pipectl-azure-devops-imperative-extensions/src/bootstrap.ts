308c92a260d38de6fe8d448436ea106a6340fd23
kroonprins <marcodebac@gmail.com>
2020-04-27 21:15:23 +0200

    dfskjj

--- packages/pipectl-azure-devops-imperative-extensions/src/bootstrap.ts
+++ packages/pipectl-azure-devops-imperative-extensions/src/bootstrap.ts
@@ -1,6 +1,7 @@
 import { registerCommand } from '@kroonprins/pipectl/dist/util/yargs'
 import { Argv } from 'yargs'
 import abandonPullRequest from './actions/abandon-pull-request'
+import completePullRequest from './actions/complete-pull-request'
 
 export default (yargs: Argv) => {
   registerCommand(
@@ -11,6 +12,14 @@ export default (yargs: Argv) => {
         yarg.positional('id', { type: 'string', description: 'todo' })
       },
       abandonPullRequest
+    ),
+    yargs.command(
+      'complete-pull-request <id>',
+      'todo',
+      (yarg) => {
+        yarg.positional('id', { type: 'string', description: 'todo' })
+      },
+      completePullRequest
     )
   )
 }
